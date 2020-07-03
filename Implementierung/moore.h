ae2cd45c3afb53bf7137d11f7ffb6be42aba8e4c
Anton Baumann <antonbaumann4@gmail.com>
2020-07-03 15:15:30 +0200

    add comment

--- Implementierung/moore.h
+++ Implementierung/moore.h
@@ -1,5 +1,6 @@
 void moore_c_iterative(uint64_t degree, uint64_t *x, uint64_t *y);
 
+// extern function name must start with underscore if compiled on mac
 #ifdef __APPLE__
 extern void moore(uint64_t degree, uint64_t *x, uint64_t *y);
 #else
