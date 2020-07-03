029a1966926bdd0020de51141a348b48368f751f
Anton Baumann <antonbaumann4@gmail.com>
2020-07-03 15:12:47 +0200

    WIP

--- Implementierung/moore.h
+++ Implementierung/moore.h
@@ -1,1 +1,10 @@
 void moore_c_iterative(uint64_t degree, uint64_t *x, uint64_t *y);
+
+#ifdef __APPLE__
+extern void moore(uint64_t degree, uint64_t *x, uint64_t *y);
+#else
+extern void _moore(uint64_t degree, uint64_t *x, uint64_t *y);
+extern void moore(uint64_t degree, uint64_t *x, uint64_t *y) {
+    _moore(degree, x, y);
+}
+#endif
