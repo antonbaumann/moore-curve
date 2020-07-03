42f61a5a39ba088b1676707cba890b0a14877eda
Anton Baumann <antonbaumann4@gmail.com>
2020-07-03 15:12:47 +0200

    WIP

--- Implementierung/main.h
+++ Implementierung/main.h
@@ -5,5 +5,3 @@ void moore_asm(long degree, uint64_t *x, uint64_t *y);
 long parse_degree(char *);
 
 int write_svg(char *filename, uint64_t *x, uint64_t *y, unsigned int degree);
-
-extern void moore(uint64_t degree, uint64_t *x, uint64_t *y);
